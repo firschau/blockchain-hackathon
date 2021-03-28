5304594b7ca843a82989393c27bae69d0dce0076
Frank Singh <frank.singh@frachtwerk.de>
2021-03-28 14:59:35 +0200

    react frontend

--- truffle-config.js
+++ truffle-config.js
@@ -18,8 +18,11 @@
  *
  */
 
-const HDWalletProvider = require('truffle-hdwallet-provider');
-const mnemonic = 'gas october undo antenna obvious turtle sunny lazy blanket near liberty august';
+const path = require('path')
+
+const HDWalletProvider = require('truffle-hdwallet-provider')
+const mnemonic =
+  'gas october undo antenna obvious turtle sunny lazy blanket near liberty august'
 // const infuraKey = "fj4jll3k.....";
 //
 // const fs = require('fs');
@@ -49,46 +52,47 @@ module.exports = {
     //  network_id: "*",       // Any network (default: none)
     // },
 
-	development: {
-      host: "127.0.0.1",     // Localhost (default: none)
-      port: 8545,            // Standard Ethereum port (default: none)
-      network_id: "*",       // Any network (default: none)
-	  from: "0x88cbf521a38f5161e5C36935298d881983f17735",
-	  gas: 10000000,
+    development: {
+      host: '127.0.0.1', // Localhost (default: none)
+      port: 8545, // Standard Ethereum port (default: none)
+      network_id: '*', // Any network (default: none)
+      from: '0x7015B7f6C89C53eA5c1b46C2DB229f933BC9518F',
+      // gas: 10000000,
     },
     // Another network with more advanced options...
     // advanced: {
-      // port: 8777,             // Custom port
-      // network_id: 1342,       // Custom network
-      // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-      // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-      // from: <address>,        // Account to send txs from (default: accounts[0])
-      // websockets: true        // Enable EventEmitter interface for web3 (default: false)
+    // port: 8777,             // Custom port
+    // network_id: 1342,       // Custom network
+    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
+    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
+    // from: <address>,        // Account to send txs from (default: accounts[0])
+    // websockets: true        // Enable EventEmitter interface for web3 (default: false)
     // },
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
     // ropsten: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-      // network_id: 3,       // Ropsten's id
-      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
+    // network_id: 3,       // Ropsten's id
+    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
+    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     // },
 
     // Useful for private networks
     // private: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-      // network_id: 2111,   // This network is yours, in the cloud.
-      // production: true    // Treats this network as if it was a public net. (default: false)
+    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
+    // network_id: 2111,   // This network is yours, in the cloud.
+    // production: true    // Treats this network as if it was a public net. (default: false)
     // }
 
-	volta: {
-	  provider: () => new HDWalletProvider(mnemonic, 'https://volta-rpc.energyweb.org'),
-	  network_id: 73799,
-	  gas: 7000000,
-	}
+    volta: {
+      provider: () =>
+        new HDWalletProvider(mnemonic, 'https://volta-rpc.energyweb.org'),
+      network_id: 73799,
+      gas: 7000000,
+    },
   },
 
   // Set default mocha options here, use special reporters etc.
@@ -96,20 +100,23 @@ module.exports = {
     // timeout: 100000
   },
 
-  plugins: ["truffle-contract-size"],
+  plugins: ['truffle-contract-size'],
 
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.7.6",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: '0.7.6', // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      settings: {          // See the solidity docs for advice about optimization and evmVersion
-      optimizer: {
+      settings: {
+        // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
           enabled: true,
-          runs: 10
+          runs: 10,
         },
-        evmVersion: "petersburg"
-       }
-    }
-  }
+        evmVersion: 'petersburg',
+      },
+    },
+  },
+
+  contracts_build_directory: path.join(__dirname, 'app/src/contracts'),
 }
