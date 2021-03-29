720d05d2c21c263117783932b9fe15d17c13baca
Frank Singh <frank.singh@frachtwerk.de>
2021-03-29 11:14:02 +0200

    vue frontend

--- app/src/drizzleOptions.js
+++ app/src/drizzleOptions.js
@@ -1,17 +1,22 @@
-import Web3 from "web3";
-import ComplexStorage from "./contracts/ComplexStorage.json";
-import SimpleStorage from "./contracts/SimpleStorage.json";
-import TutorialToken from "./contracts/TutorialToken.json";
+import SimpleStorage from './contracts/SimpleStorage.json'
+import ComplexStorage from './contracts/ComplexStorage.json'
+import TutorialToken from './contracts/TutorialToken.json'
 
 const options = {
   web3: {
     block: false,
-    customProvider: new Web3("ws://localhost:8545"),
+    fallback: {
+      type: 'ws',
+      url: 'ws://127.0.0.1:8545',
+    },
   },
   contracts: [SimpleStorage, ComplexStorage, TutorialToken],
   events: {
-    SimpleStorage: ["StorageSet"],
+    SimpleStorage: ['StorageSet'],
   },
-};
+  polls: {
+    accounts: 15000,
+  },
+}
 
-export default options;
+export default options
