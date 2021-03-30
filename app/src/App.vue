79b198e85673a9ff0bcc8266fadf307913da011d
Frank Singh <ukdqp@student.kit.edu>
2021-03-30 12:58:25 +0200

    removed tutorial contracts, basic setup

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,56 +1,32 @@
 <template>
-    <v-app>
-        <v-app-bar app color="primary" dark>
-            <div class="d-flex align-center">
-                <v-img
-                    alt="Vuetify Logo"
-                    class="shrink mr-2"
-                    contain
-                    src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-                    transition="scale-transition"
-                    width="40"
-                />
-
-                <v-img
-                    alt="Vuetify Name"
-                    class="shrink mt-1 hidden-sm-and-down"
-                    contain
-                    min-width="100"
-                    src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-                    width="100"
-                />
-            </div>
-
-            <v-spacer></v-spacer>
-
-            <v-btn
-                href="https://github.com/vuetifyjs/vuetify/releases/latest"
-                target="_blank"
-                text
-            >
-                <span class="mr-2">Latest Release</span>
-                <v-icon>mdi-open-in-new</v-icon>
-            </v-btn>
-        </v-app-bar>
+    <v-app v-if="isDrizzleInitialized">
+        <v-app-bar app color="primary" dark> </v-app-bar>
 
         <v-main>
-            <HelloWorld />
+            <v-container>
+                <drizzle-account units="Ether" :precision="2" />
+
+                <drizzle-contract
+                    contractName="IdentityContract"
+                    method="marketAuthority"
+                    label="Market Authority"
+                />
+            </v-container>
         </v-main>
     </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
+import { mapGetters } from 'vuex'
 export default {
     name: 'App',
-
-    components: {
-        HelloWorld,
-    },
 
     data: () => ({
         //
     }),
+
+    computed: {
+        ...mapGetters('drizzle', ['isDrizzleInitialized']),
+    },
 }
 </script>
