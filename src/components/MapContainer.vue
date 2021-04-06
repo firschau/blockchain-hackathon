af1a3ec41fc278d3a5353c420fe5650088996586
Leo <ubqok@kit.edu>
2021-04-07 01:06:39 +0200

    polygon in BW focuse still dosent work -gps data difficult

---
+++ src/components/MapContainer.vue
@@ -0,0 +1,33 @@
+<script>
+import GeoJSON from 'ol/format/GeoJSON'
+
+import Map from 'ol/Map'
+
+// this is a simple triangle over the atlantic ocean
+const data = {
+    type: 'Feature',
+    properties: {},
+    geometry: {
+        type: 'Polygon',
+        coordinates: [
+            [
+                [10.6360143, 49.10675],
+                [10.16765, 49.09879],
+                [9.0744, 48.991082],
+                [8.33, 49.14],
+            ],
+        ],
+    },
+}
+
+export default {
+    // ...
+
+    mounted() {
+        new Map({
+            // ...
+            // ...
+        })
+    },
+}
+</script>
--- src/views/MapTest.vue
+++ src/views/MapTest.vue
@@ -11,18 +11,53 @@ import View from 'ol/View'
 import Map from 'ol/Map'
 import TileLayer from 'ol/layer/Tile'
 import OSM from 'ol/source/OSM'
+import VectorLayer from 'ol/layer/Vector'
+import VectorSource from 'ol/source/Vector'
+import GeoJSON from 'ol/format/GeoJSON'
 
 // importing the OpenLayers stylesheet is required for having
 // good looking buttons!
 import 'ol/ol.css'
-
+// this is a simple triangle over the atlantic ocean
+const data = {
+    type: 'Feature',
+    properties: {},
+    geometry: {
+        type: 'Polygon',
+        coordinates: [
+            [
+                [10.6360143, 49.10675],
+                [10.16765, 49.09879],
+                [9.0744, 48.991082],
+                [8.33, 49.14],
+            ],
+        ],
+    },
+}
 export default {
     name: 'MapTest',
     components: {},
     props: {},
+
     mounted() {
+        const feature = new GeoJSON().readFeature(data, {
+            // this is required since GeoJSON uses latitude/longitude,
+            // but the map is rendered using “Web Mercator”
+            featureProjection: 'EPSG:3857',
+        })
+        const vectorLayer = new VectorLayer({
+            source: new VectorSource({
+                features: [feature],
+            }),
+        })
+
         // this is where we create the OpenLayers map
         new Map({
+            view: new View({
+                zoom: 6,
+                center: [9.99, 46.91],
+                constrainResolution: true,
+            }),
             // the map will be created using the 'map-root' ref
             target: this.$refs['map-root'],
             layers: [
@@ -30,14 +65,10 @@ export default {
                 new TileLayer({
                     source: new OSM(), // tiles are served by OpenStreetMap
                 }),
+                vectorLayer,
             ],
 
             // the map view will initially show the whole world
-            view: new View({
-                zoom: 0,
-                center: [0, 0],
-                constrainResolution: true,
-            }),
         })
     },
 }
