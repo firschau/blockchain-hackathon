aeb65066d8315c87ffe053aae98e71cef32cac18
Frank Singh <ukdqp@student.kit.edu>
2021-04-13 15:08:31 +0200

    format

--- src/utils/drizzle.js
+++ src/utils/drizzle.js
@@ -1,31 +1,20 @@
 import store from '@/store'
 
 export const cacheCall = (contract, method, ...args) => {
-    return store.getters['drizzle/drizzleInstance'].contracts[contract].methods[
-        method
-    ].cacheCall(...args)
+    return store.getters['drizzle/drizzleInstance'].contracts[contract].methods[method].cacheCall(...args)
 }
 
 export const call = (contract, method, ...args) => {
-    return store.getters['drizzle/drizzleInstance'].contracts[contract].methods[
-        method
-    ](...args).call()
+    return store.getters['drizzle/drizzleInstance'].contracts[contract].methods[method](...args).call()
 }
 
 export const cacheSend = (contract, method, value, ...args) => {
-    return store.getters['drizzle/drizzleInstance'].contracts[contract].methods[
-        method
-    ].cacheSend(value, ...args)
+    return store.getters['drizzle/drizzleInstance'].contracts[contract].methods[method].cacheSend(value, ...args)
 }
 
 export const send = (contract, method, value, ...args) => {
-    return store.getters['drizzle/drizzleInstance'].contracts[contract].methods[
-        method
-    ](value).send(...args)
+    return store.getters['drizzle/drizzleInstance'].contracts[contract].methods[method](value).send(...args)
 }
 
 export const getNewContract = (jsonInterface, address) =>
-    new store.getters['drizzle/drizzleInstance'].web3.eth.Contract(
-        jsonInterface.abi,
-        address
-    )
+    new store.getters['drizzle/drizzleInstance'].web3.eth.Contract(jsonInterface.abi, address)
