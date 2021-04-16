9af431615c749a6a8409e3976e2aa02087f0f4ac
firschau <udxho@student.kit.edu>
2021-04-16 22:56:06 +0200

    updated home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,18 @@
     <v-container>
         <v-row class="text-center">
             <v-col justify="center">
-                <h1 style="margin-top: 30px">
+                <h1 style="margin: 30px">
                     BLOCKCHAIN-BASED <br />
                     ENERGY MARKETS.
                 </h1>
                 <p>POWERED BY</p>
                 <img :src="require('../assets/ethereum.svg')" alt="Ethereum Logo" width="40" />
-                <p>
-                    This app provides a first prototype of a blockchain-based<br />energy market, leveraging the EnBW
-                    B2E2 architecture.
+                <p align="justify" style="margin: 10px 100px 0px 100px">
+                    This app provides a first prototype of a blockchain-based energy market, implementing the EnBW B2E2
+                    architecture. It allows performing the setup of the various authorities proposed in the EnBW
+                    whitepaper, creating new electricity consumption or generation plants and enables authorities to
+                    provide claims to these plants. This prototypical implementation utilizes a local Ganache blockchain
+                    and a JSON-Server. For detailed guidance please refer to the following resources:
                 </p>
             </v-col>
         </v-row>
@@ -20,14 +23,12 @@
                     style="margin: 20px"
                     target="_blank"
                     href="https://it-architecture.enbw.com/whitepaper-energy-token-model/"
-                    >B2E2 Whitepaper</a
                 >
-                <a
-                    style="margin: 20px"
-                    target="_blank"
-                    href="https://it-architecture.enbw.com/energy-token-model-smart-contract-code/"
-                    >B2E2 Repository</a
-                >
+                    <b>EnBW B2E2 Whitepaper</b>
+                </a>
+                <a style="margin: 20px" target="_blank" href="https://git.scc.kit.edu/ukdqp/blockchain-hackathon-enbw">
+                    <b>Git Repository</b>
+                </a>
             </v-col>
         </v-row>
     </v-container>
