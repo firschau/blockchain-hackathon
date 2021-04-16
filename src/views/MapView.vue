e95bce51ac9c4267d6a4fb51297a4616a2170079
Leo <ubqok@kit.edu>
2021-04-16 15:45:40 +0200

    first point with ID when clicked

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -3,6 +3,10 @@
         <v-card class="ma-10">
             <v-card-title style="">Plant Locations</v-card-title>
             <div ref="map-root" style="width: 100%; height: 600px"></div>
+            <div id="popup" class="ol-popup">
+                <a href="#" id="popup-closer" class="ol-popup-closer"></a>
+                <div id="popup-content"></div>
+            </div>
         </v-card>
         <!-- 
         <v-row class="text-center">
@@ -17,7 +21,19 @@ import Map from 'ol/Map'
 import TileLayer from 'ol/layer/Tile'
 import OSM from 'ol/source/OSM'
 import { fromLonLat } from 'ol/proj'
+var container = document.getElementById('popup')
+var content = document.getElementById('popup-content')
+var closer = document.getElementById('popup-closer')
 
+// import ol from 'ol'
+//import Geom from 'ol/geom'
+
+import Feature from 'ol/Feature'
+import LayerVector from 'ol/layer/Vector'
+import SourceVector from 'ol/source/Vector'
+import Point from 'ol/geom/Point'
+import Overlay from 'ol/Overlay'
+//import Source from 'ol/source'
 export default {
     name: 'MapView',
 
@@ -31,7 +47,8 @@ export default {
     mounted() {
         const karlsruheGPS = [8.40436, 49.013941] //first Long then Lat !
         // this is where we create the OpenLayers map
-        new Map({
+
+        var map = new Map({
             // the map will be created using the 'map-root' ref
             target: this.$refs['map-root'],
             layers: [
@@ -48,6 +65,55 @@ export default {
                 constrainResolution: true,
             }),
         })
+
+        var layer = new LayerVector({
+            source: new SourceVector({
+                features: [
+                    new Feature({
+                        geometry: new Point(fromLonLat(karlsruheGPS)),
+                    }),
+                ],
+            }),
+        })
+        map.addLayer(layer)
+
+        var overlay = new Overlay({
+            element: container,
+            autoPan: true,
+            autoPanAnimation: {
+                duration: 250,
+            },
+        })
+        map.addOverlay(overlay)
+
+        closer.onclick = function () {
+            overlay.setPosition(undefined)
+            closer.blur()
+            return false
+        }
+
+        map.on('singleclick', function (event) {
+            if (map.hasFeatureAtPixel(event.pixel) === true) {
+                var coordinate = event.coordinate
+
+                content.innerHTML = '<b>Plant ID-23323</b>'
+                overlay.setPosition(coordinate)
+            } else {
+                overlay.setPosition(undefined)
+                closer.blur()
+            }
+        })
+
+        // var layer = new ol.layer.Vector({
+        //     source: new ol.source.Vector({
+        //         features: [
+        //             new ol.Feature({
+        //                 geometry: new ol.geom.Point(ol.proj.fromLonLat([4.35247, 50.84673])),
+        //             }),
+        //         ],
+        //     }),
+        // })
+        // map.addLayer(layer)
     },
 
     methods: {
